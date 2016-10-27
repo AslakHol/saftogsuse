f748c1d7eb5f2d6c4b2636c05d9e5e14af777f14
Jonas Bjøralt Giske <jonasbgi@stud.ntnu.no>
2016-10-27 13:14:13 +0200

    Added functions to hide and show text. Added event-listeners to imagemap

--- scripts/presses_click.js
+++ scripts/presses_click.js
@@ -1,3 +1,10 @@
-/*function testMap(){
-	alert("Shallabais");
-}*/
+function testMap(showText){
+	newBox = document.getElementById(showText);
+	newBox.style.visibility="visible";
+	alert("wohoo");
+}
+function hideItem(item){
+	document.getElementById(item).style.visibility="hidden";
+}
+document.getElementById("testMap").addEventListener("click", testMap("testText"));
+//hideItem("testText");
