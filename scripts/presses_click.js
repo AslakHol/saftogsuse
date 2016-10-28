4c32ca482d587fcaba5464769d6dfc3387ec5403
Jonas Bjøralt Giske <jonasbgi@stud.ntnu.no>
2016-10-28 09:20:46 +0200

    Script til eplepresse fungerer fint

--- scripts/presses_click.js
+++ scripts/presses_click.js
@@ -1,10 +1,10 @@
 function testMap(showText){
 	newBox = document.getElementById(showText);
 	newBox.style.visibility="visible";
-	alert("wohoo");
+	
 }
 function hideItem(item){
 	document.getElementById(item).style.visibility="hidden";
 }
 document.getElementById("testMap").addEventListener("click", testMap("testText"));
-//hideItem("testText");
+hideItem("testText");
