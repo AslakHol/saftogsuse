64a252ea54c81ffad643c51ff8487b43753f31ea
Ine Lofthus <inelofthus@gmail.com>
2016-10-29 16:32:23 +0200

    Diverse endringer i navbar (add_header_footer) og tilhørende css: facebooklogo, logo, spacing

---
+++ scripts/img_script.js
@@ -0,0 +1,5 @@
+var fblogo = document.getElementById("fb_logo_img");
+fblogo.src = "img/_f_logo_online/png/FB-f-Logo__grey_29.png";
+fblogo.addEventListener("mouseover", function(){
+	fblogo.src = "img/_f_logo_online/png/FB-f-Logo__blue_29.png";
+});
