838625ac7f2148da45a694d83f11ba6500d1822d
Aslak Hollund <aslakhol@gmail.com>
2016-11-02 14:04:51 +0100

    basic slideshow functionality finished

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,41 +1,25 @@
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
-const currentSlide = document.createElement("img")
+const imgElement = document.createElement("img")
 const imgPaths = ["img/slides/slide_1.JPG", "img/slides/slide_2.JPG", "img/slides/slide_3.JPG", "img/slides/slide_4.JPG"]
-// const imgtest = document.getElementById("imgtest")
+
+let slideIndex = 0
 
 
-if (currentSlide){
-  currentSlide.setAttribute('src', imgPaths[0])
-  currentSlide.setAttribute("onload", "insertImage()")
-  console.log(currentSlide + "setting up currentSlide")
-  console.log(currentSlide)
+if (imgElement){
+  imgElement.setAttribute('src', imgPaths[slideIndex])
+  imgElement.setAttribute("onload", "insertImage()")
+  setInterval(switchImage, 3000)
 }
 
 function insertImage(){
-  console.log("inserting image")
-  ctx.drawImage(currentSlide, 10, 10)
+  ctx.drawImage(imgElement, 0, 0, 700, 525)
 }
 
-if (ctx){
-  // imgtest.setAttribute('src', imgPaths[0])
-
-  /*
-  ctx.beginPath()
-  ctx.strokeStyle = "black"
-  ctx.arc(250, 250, 100, 0, 2 * Math.PI)
-  ctx.stroke()
-
-  ctx.beginPath()
-  ctx.arc(210,215,5,0,2*Math.PI)
-  ctx.stroke()
-
-  ctx.beginPath()
-  ctx.arc(290, 215, 5, 0, 2 * Math.PI)
-  ctx.stroke()
-
-  ctx.beginPath()
-  ctx.arc(250, 260, 40, 0, Math.PI, false)
-  ctx.stroke()
-  */
+function switchImage(){
+  slideIndex ++
+  if (slideIndex >= imgPaths.length){
+    slideIndex = 0
+  }
+  imgElement.setAttribute('src', imgPaths[slideIndex])
 }
