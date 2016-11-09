568804084af2d0b5bb428e3fec06f5fb6ac1f51f
Aslak Hollund <aslakhol@gmail.com>
2016-11-09 23:04:35 +0100

    updated some more documentation

--- scripts/presses_click.js
+++ scripts/presses_click.js
@@ -1,15 +1,11 @@
 /*
-File name: presses_click.js
+FILE NAME: presses_click.js
+WRITTEN BY: Jonas Bjøralt Giske
+WHEN: November 2016
+PURPOSE: Power the interactive applepress at eplepresser.html.
 
-Author: Jonas Bjøralt Giske
-
-Made: October 2016
-Last edit: 9th. November 2016
-
-Purpose: Power the interactive applepress at eplepresser.html. 
-
-Summary: Makes elements of the same class invisible (visibility hidden) and keeps one element visible, or hides an element.
-All variables and constants are based upon HTML-fed-arguments; this code may be reused for a different HTML. 
+This makes elements of the same class invisible (visibility hidden) and keeps one element visible, or hides an element.
+All variables and constants are based upon HTML-fed-arguments; this code may be reused for a different HTML.
 */
 function testMap(showText){
 	newBox = document.getElementById(showText);
@@ -24,7 +20,7 @@ function hideAllBut(showItem, className){
 	}
 	visibleBox = document.getElementById(showItem);
 	visibleBox.style.visibility="visible";
-	
+
 }
 function hideItem(elmntID){
 	document.getElementById(elmntID).style.visibility="hidden";
