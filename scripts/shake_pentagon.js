0b4ae9e4a1e72eaff2a45c63405e44762125ad48
MountainPancake <z.b.svela@gmail.com>
2016-11-09 23:13:19 +0100

    Fixed comments and config in shake_pentagon

--- scripts/shake_pentagon.js
+++ scripts/shake_pentagon.js
@@ -1,8 +1,16 @@
 
-/*Created by Zawadi Berg Svela, november 2016
-This script makes the logo on the index.html site shake on mouseover.
-It is meant as an easter egg in addition to the mandatory javascripts.
+/*
+FILE NAME: shake_pentagon.js
+WRITTEN BY: Zawadi Berg Svela 
+WHEN: November 2016
+Purpose: This script makes the logo on the landing page shake on mouseover. 
+It is meant as a funny easter egg in addition to the mandatory javascripts.
 */
+
+// Config for shaking
+const number_of_shakes = 40
+const shake_amplitude_in_pixels = 1
+const milliseconds_between_shakes = 0.5
 
 const pentagon = document.getElementById('logo').firstElementChild
 pentagon.addEventListener('mouseover',(e) => {
@@ -12,7 +20,7 @@ function shake(pentagon){
 	let count = 0
 	let shake = setInterval(() => {
 		let up
-		if(count == 40){
+		if(count == number_of_shakes){
 			clearInterval(shake)
 			pentagon.style.left = '50%'
 			pentagon.style.top = '50%'
@@ -20,8 +28,8 @@ function shake(pentagon){
 			count++
 			let upSign = Math.round(Math.random()*2)-1
 			let leftSign = Math.round(Math.random()*2)-1
-			pentagon.style.left = 50 + leftSign*1 + '%'
-			pentagon.style.top = 50 + upSign*1 + '%'
+			pentagon.style.left = 50 + leftSign*shake_amplitude_in_pixels + '%'
+			pentagon.style.top = 50 + upSign*shake_amplitude_in_pixels + '%'
 		}
-	},0.5)
+	},milliseconds_between_shakes)
 }
