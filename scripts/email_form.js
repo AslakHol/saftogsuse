771a73131161c626648cdec8c68d76c1f50c110a
Ine Lofthus <inelofthus@gmail.com>
2016-11-11 14:09:55 +0100

    Velge mottaker i form

--- scripts/email_form.js
+++ scripts/email_form.js
@@ -6,13 +6,24 @@ PURPOSE: javascript that autofills email with name, subject and message
 */
 
 // function autofills name, subject, and message into email
-function autofillEmail() {
-    var name = document.getElementById("name").value;
-    var subject = document.getElementById("subject").value;
-    var message = document.getElementById("message").value;
 
-    var body = encodeURI(message)+"%0D%0A"+"%0D%0A"+encodeURI(name);
+function yo(){
+	window.onload = function(){
+		function autofillEmail() {
+		    var name = document.getElementById("name").value;
+		    var subject = document.getElementById("subject").value;
+		    var message = document.getElementById("message").value;
+		    var webmasterChoice = document.getElementById("webmaster");
+		    if (webmasterChoice.checked){
+		    	var receiver = "webmaster@saftogsuse.no";
+		    } else{
+		    	var receiver = "kontakt@saftogsuse.no";
+		    }
 
-    window.location.href='mailto:kontakt@saftogsuse.no?subject='+encodeURI(subject)+'&body='+body;
-    return false;
+		    var body = encodeURI(message)+"%0D%0A"+"%0D%0A"+encodeURI(name);
+
+		    window.location.href='mailto:'+receiver+'?subject='+encodeURI(subject)+'&body='+body;
+		    return false;
+		}
+	};
 }
