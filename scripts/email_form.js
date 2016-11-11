41dbb853a805935d814209918adac5681c388362
Jonas Bjøralt Giske <jonasbgi@stud.ntnu.no>
2016-11-11 15:00:19 +0100

    Moved script to external file. submit-button now has an onclick

--- scripts/email_form.js
+++ scripts/email_form.js
@@ -7,23 +7,22 @@ PURPOSE: javascript that autofills email with name, subject and message
 
 // function autofills name, subject, and message into email
 
-function yo(){
-	window.onload = function(){
-		function autofillEmail() {
-		    var name = document.getElementById("name").value;
-		    var subject = document.getElementById("subject").value;
-		    var message = document.getElementById("message").value;
-		    var webmasterChoice = document.getElementById("webmaster");
-		    if (webmasterChoice.checked){
-		    	var receiver = "webmaster@saftogsuse.no";
-		    } else{
-		    	var receiver = "kontakt@saftogsuse.no";
-		    }
-
-		    var body = encodeURI(message)+"%0D%0A"+"%0D%0A"+encodeURI(name);
-
-		    window.location.href='mailto:'+receiver+'?subject='+encodeURI(subject)+'&body='+body;
-		    return false;
-		}
-	};
-}
+function autofillEmail() {
+		console.log("autoFill ran");
+          var name = document.getElementById("name").value;
+          var subject = document.getElementById("subject").value;
+          var message = document.getElementById("message").value;
+          var webmasterChoice = document.getElementById("webmaster");
+          if (webmasterChoice.checked){
+            var receiver = "webmaster@saftogsuse.no";
+			console.log("WebM")
+          } else{
+            var receiver = "kontakt@saftogsuse.no";
+			console.log("SS")
+          }
+		
+          var body = encodeURI(message)+"%0D%0A"+"%0D%0A"+encodeURI(name);
+			console.log(body)
+          window.location.href='mailto:'+receiver+'?subject='+encodeURI(subject)+'&body='+body;
+        return false;
+        }
